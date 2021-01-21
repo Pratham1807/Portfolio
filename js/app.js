fc9037ade156f86154be7b790c761932ec7ebe36
Pratham1807 <pratham1807@gmail.com>
2021-01-22 02:13:46 +0530

    adding contact section

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const tl = gsap.timeline({defaults: {ease: "power1.out"}})
+// const tl = gsap.timeline({defaults: {ease: "power1.out"}})
 
-tl.to('.text', {y:'0%', duration:1, stagger: 0.25})
+// tl.to('.text', {y:'0%', duration:1, stagger: 0.25})
 
-tl.to('.slider', {y: "-100%", duration: 1.5, delay:1.5})
+// tl.to('.slider', {y: "-100%", duration: 1.5, delay:1.5})
 
-tl.to('.intro', {y:'-100%', duration: 1}, '-=1')
+// tl.to('.intro', {y:'-100%', duration: 1}, '-=1')
 
-tl.fromTo('.unhide-animation', {opacity: '0%'}, {opacity: '100%', duration: 1})
+// tl.fromTo('.unhide-animation', {opacity: '0%'}, {opacity: '100%', duration: 1})
 
-setTimeout(function() {
-    document.getElementsByClassName("no-display").item(0).style.display = 'block'
-  }, 5000);
+// setTimeout(function() {
+//     document.getElementsByClassName("no-display").item(0).style.display = 'block'
+//   }, 5000);
 
 //smooth scrolling
 $('.menu li a').on('click', function(e){
